e51fb8eb8393026c5f35c362c0f23ef7b9961fbe
Lucas de Castro Oliveira <lcastro.oliveira@gmail.com>
2017-08-01 15:44:15 -0300

    Add simple form example S14L125

--- WebContent/WEB-INF/view/main-menu.jsp
+++ WebContent/WEB-INF/view/main-menu.jsp
@@ -4,5 +4,7 @@
 		<h2>Spring MVC Demo - Home Page</h2>
 		<hr>
 		<a href="hello/showForm">Hello World form</a>
+		<br>
+		<a href="student/new">Student form</a>
 	</body>
 </html>
---
+++ src/com/luv2code/springdemo/mvc/Student.java
@@ -0,0 +1,26 @@
+package com.luv2code.springdemo.mvc;
+
+//Just a POJO for example
+public class Student {
+
+	private String firstName, lastName;
+	
+	public Student() {
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
