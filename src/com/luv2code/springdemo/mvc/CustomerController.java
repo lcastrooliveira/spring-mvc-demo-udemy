f05d0a32478a35d1fa6447ec912b57c74fc538e9
Lucas de Castro Oliveira <lcastro.oliveira@gmail.com>
2017-08-01 22:30:13 -0300

    Handle validation white space corner case with @InitBinder S15L148

--- src/com/luv2code/springdemo/mvc/CustomerController.java
+++ src/com/luv2code/springdemo/mvc/CustomerController.java
@@ -2,15 +2,28 @@ package com.luv2code.springdemo.mvc;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+	
+	// add an init binder to convert trim string inputs
+	// remove leading and trailing whitespace
+	//resolve issue for our validation
+	
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		StringTrimmerEditor ste = new StringTrimmerEditor(true);
+		dataBinder.registerCustomEditor(String.class, ste);
+	}
 
 	@RequestMapping("/new")
 	public String newCustomer(Model theModel) {
